476fa219bcff01bfd52ee0088b0f773cf7f0efd1
parkgunwook <ss944161269441@gmail.com>
2025-12-22 16:45:24 +0900

    Initial commit

---
+++ src/main/java/ahubbe/ahubbe/AHubBeApplication.java
@@ -0,0 +1,13 @@
+package ahubbe.ahubbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AHubBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AHubBeApplication.class, args);
+    }
+
+}
