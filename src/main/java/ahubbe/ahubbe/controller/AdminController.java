881deb644e301e79576668ebed2ba8cf3ad61dad
parkgunwook0617 <ss944161269441@gmail.com>
2025-12-27 11:21:59 +0900

    [test]: add manual data update (#9)
    * [test]: add manual data update
    
    * [test]: add manual data update

--- src/main/java/ahubbe/ahubbe/controller/AdminController.java
+++ src/main/java/ahubbe/ahubbe/controller/AdminController.java
@@ -1,10 +1,12 @@
 package ahubbe.ahubbe.controller;
 
+import ahubbe.ahubbe.dto.AnimeDto;
 import ahubbe.ahubbe.entity.AnimationInformation;
 import ahubbe.ahubbe.repository.AdminRepository;
 import ahubbe.ahubbe.service.Admin.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class AdminController {
     @GetMapping(path = "/inquiry")
     public List<AnimationInformation> inquiry(String year, String quarter) {
         return adminService.saveAnimeData(year, quarter, "title");
+    }
+
+    @GetMapping(path = "/selfInquiry")
+    public AnimationInformation selfInquiry(@ModelAttribute AnimeDto animeDto, String year, String quarter) {
+        return adminService.saveSingleAnimeData(animeDto, year, quarter);
     }
 }
---
+++ src/test/java/ahubbe/ahubbe/AdminServiceTest.java
@@ -0,0 +1,34 @@
+package ahubbe.ahubbe;
+
+import ahubbe.ahubbe.dto.AnimeDto;
+import ahubbe.ahubbe.entity.AnimationInformation;
+import ahubbe.ahubbe.repository.AdminRepository;
+import ahubbe.ahubbe.service.Admin.AdminService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class AdminServiceTest {
+
+    @Autowired
+    AdminService adminService;
+
+    @Autowired
+    AdminRepository adminRepository;
+
+    @Test
+    @DisplayName("수동 저장이 DB에 잘 저장되는지 확인")
+    void selfInquiryTest() {
+        AnimeDto animeDto = new AnimeDto("testTitle", "testKeyVisual", List.of("test"));
+        String testYear = "2025";
+        String testQuarter = "1";
+        adminService.saveSingleAnimeData(animeDto, testYear, testQuarter);
+    }
+}
