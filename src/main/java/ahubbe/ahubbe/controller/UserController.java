de386fcb95fdbb9e4640f3741dec752857aae0f5
parkgunwook0617 <ss944161269441@gmail.com>
2026-01-04 16:20:27 +0900

    [feat]: add favoriteAnimation method and queyr genre (#20)
    * [fix]: fix cors setting
    
    * [feat]: add save and get favorite animation

--- src/main/java/ahubbe/ahubbe/controller/UserController.java
+++ src/main/java/ahubbe/ahubbe/controller/UserController.java
@@ -2,39 +2,66 @@ package ahubbe.ahubbe.controller;
 
 import ahubbe.ahubbe.entity.AnimationInformation;
 import ahubbe.ahubbe.repository.AnimationRepository;
+import ahubbe.ahubbe.service.User.UserService;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final AnimationRepository animationRepository;
+    private final UserService userService;
 
     @GetMapping("/findAll")
     public Iterable<AnimationInformation> findAll() {
-        return animationRepository.findAll();
+        return userService.importAnimationInformation();
     }
 
     @GetMapping("/findAllByTitle")
     public Iterable<AnimationInformation> findAllByTitle(String title) {
-        return animationRepository.findAllByTitleContaining(title);
+        return userService.importAllAnimationInformationByTitle(title);
     }
 
     @GetMapping("/findAllByGenre")
     public Iterable<AnimationInformation> findAllByGenre(String genre) {
-        return animationRepository.findAllByGenreListContaining(genre);
+        return userService.importAnimationInformationByGenre(genre);
     }
 
     @GetMapping("/findAllByReleaseYear")
     public Iterable<AnimationInformation> findAllByReleaseYear(String releaseYear) {
-        return animationRepository.findAllByReleaseYearContaining(releaseYear);
+        return userService.importAnimationInformationByReleaseYear(releaseYear);
     }
 
     @GetMapping("/findAllByReleaseQuarter")
     public Iterable<AnimationInformation> findAllByReleaseQuarter(String releaseQuarter) {
-        return animationRepository.findAllByReleaseQuarterContaining(releaseQuarter);
+        return userService.importAnimationInformationByReleaseQuarter(releaseQuarter);
+    }
+
+    @GetMapping("/findSingleAnimation")
+    public Optional<AnimationInformation> findSingleAnimation(String title) {
+        return userService.importSingleAnimationInformationByTitle(title);
+    }
+
+    @PostMapping("/saveFavorite")
+    public ResponseEntity<?> saveFavoriteAnimationInformation(
+            @CookieValue(name = "accessToken") String token, String title) {
+        userService.addFavorite(token, title);
+
+        return ResponseEntity.ok("성공적으로 저장되었습니다.");
+    }
+
+    @GetMapping("/getFavorite")
+    public List<AnimationInformation> getFavoriteAnimationInformation(
+            @CookieValue(name = "accessToken") String token) {
+        return userService.getFavorite(token);
+    }
+
+    @GetMapping("/getGenreList")
+    public List<String> getGenreList() {
+        return userService.getGenreList();
     }
 }
--- src/main/java/ahubbe/ahubbe/service/User/UserService.java
+++ src/main/java/ahubbe/ahubbe/service/User/UserService.java
@@ -1,9 +1,14 @@
 package ahubbe.ahubbe.service.User;
 
 import ahubbe.ahubbe.entity.AnimationInformation;
+import ahubbe.ahubbe.entity.User;
 import ahubbe.ahubbe.repository.AnimationRepository;
-import java.util.List;
+import ahubbe.ahubbe.repository.UserRepository;
+import ahubbe.ahubbe.service.Auth.JwtTokenProvider;
+import jakarta.transaction.Transactional;
+import java.util.*;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,13 +16,15 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final AnimationRepository animationRepository;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public List<AnimationInformation> importAnimationInformation() {
 
         return animationRepository.findAll();
     }
 
-    public List<AnimationInformation> importAnimationInformationByTitle(String Title) {
+    public List<AnimationInformation> importAllAnimationInformationByTitle(String Title) {
 
         return animationRepository.findAllByTitleContaining(Title);
     }
@@ -36,5 +43,53 @@ public class UserService {
             String ReleaseQuarter) {
 
         return animationRepository.findAllByReleaseQuarterContaining(ReleaseQuarter);
+    }
+
+    public Optional<AnimationInformation> importSingleAnimationInformationByTitle(String Title) {
+
+        return animationRepository.findByTitle(Title);
+    }
+
+    @Transactional
+    public void addFavorite(String token, String title) {
+        if (!jwtTokenProvider.validateToken(token)) {
+            throw new RuntimeException("유효하지 않은 토큰입니다.");
+        }
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+        String userId = authentication.getName();
+
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        user.getFavoriteAnimations().add(animationRepository.findByTitle(title).get());
+    }
+
+    public List<AnimationInformation> getFavorite(String token) {
+        if (!jwtTokenProvider.validateToken(token)) {
+            throw new RuntimeException("유효하지 않은 토큰입니다.");
+        }
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+        String userId = authentication.getName();
+
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        return user.getFavoriteAnimations();
+    }
+
+    public ArrayList<String> getGenreList() {
+        List<AnimationInformation> list = animationRepository.findAll();
+        Set genreList = new HashSet();
+        for (AnimationInformation animationInformation : list) {
+            genreList.addAll(animationInformation.getGenreList());
+        }
+
+        return new ArrayList<String>(genreList);
     }
 }
