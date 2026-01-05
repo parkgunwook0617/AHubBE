d90527b8837f3f9ad1a2fbf6dde80b698e48dcff
parkgunwook <ss944161269441@gmail.com>
2026-01-05 09:58:51 +0900

    [feat]: add auth Method and like Method

--- src/main/java/ahubbe/ahubbe/service/User/UserService.java
+++ src/main/java/ahubbe/ahubbe/service/User/UserService.java
@@ -5,6 +5,7 @@ import ahubbe.ahubbe.entity.User;
 import ahubbe.ahubbe.repository.AnimationRepository;
 import ahubbe.ahubbe.repository.UserRepository;
 import ahubbe.ahubbe.service.Auth.JwtTokenProvider;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import java.util.*;
 import lombok.AllArgsConstructor;
@@ -24,30 +25,30 @@ public class UserService {
         return animationRepository.findAll();
     }
 
-    public List<AnimationInformation> importAllAnimationInformationByTitle(String Title) {
+    public List<AnimationInformation> importAllAnimationInformationByTitle(String title) {
 
-        return animationRepository.findAllByTitleContaining(Title);
+        return animationRepository.findAllByTitleContaining(title);
     }
 
-    public List<AnimationInformation> importAnimationInformationByGenre(String Genre) {
+    public List<AnimationInformation> importAnimationInformationByGenre(String genre) {
 
-        return animationRepository.findAllByGenreListContaining(Genre);
+        return animationRepository.findAllByGenreListContaining(genre);
     }
 
-    public List<AnimationInformation> importAnimationInformationByReleaseYear(String ReleaseYear) {
+    public List<AnimationInformation> importAnimationInformationByReleaseYear(String releaseYear) {
 
-        return animationRepository.findAllByReleaseYearContaining(ReleaseYear);
+        return animationRepository.findAllByReleaseYearContaining(releaseYear);
     }
 
     public List<AnimationInformation> importAnimationInformationByReleaseQuarter(
-            String ReleaseQuarter) {
+            String releaseQuarter) {
 
-        return animationRepository.findAllByReleaseQuarterContaining(ReleaseQuarter);
+        return animationRepository.findAllByReleaseQuarterContaining(releaseQuarter);
     }
 
-    public Optional<AnimationInformation> importSingleAnimationInformationByTitle(String Title) {
+    public Optional<AnimationInformation> importSingleAnimationInformationByTitle(String title) {
 
-        return animationRepository.findByTitle(Title);
+        return animationRepository.findByTitle(title);
     }
 
     @Transactional
@@ -64,10 +65,16 @@ public class UserService {
                         .findById(userId)
                         .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
 
-        user.getFavoriteAnimations().add(animationRepository.findByTitle(title).get());
+        user.getFavoriteAnimations()
+                .add(
+                        animationRepository
+                                .findByTitle(title)
+                                .orElseThrow(
+                                        () -> new EntityNotFoundException("애니메이션 정보를 찾을 수 없습니다.")));
     }
 
-    public List<AnimationInformation> getFavorite(String token) {
+    @Transactional
+    public void removeFavorite(String token, String title) {
         if (!jwtTokenProvider.validateToken(token)) {
             throw new RuntimeException("유효하지 않은 토큰입니다.");
         }
@@ -78,14 +85,36 @@ public class UserService {
         User user =
                 userRepository
                         .findById(userId)
-                        .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+                        .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다."));
+
+        user.getFavoriteAnimations()
+                .remove(
+                        animationRepository
+                                .findByTitle(title)
+                                .orElseThrow(
+                                        () -> new EntityNotFoundException("애니메이션 정보를 찾을 수 없습니다.")));
+    }
+
+    public Set<AnimationInformation> getFavorite(String token) {
+        if (!jwtTokenProvider.validateToken(token)) {
+            throw new RuntimeException("유효하지 않은 토큰입니다.");
+        }
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+        String userId = authentication.getName();
+
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new EntityNotFoundException("사용자를 찾을 수 없습니다."));
 
         return user.getFavoriteAnimations();
     }
 
     public ArrayList<String> getGenreList() {
         List<AnimationInformation> list = animationRepository.findAll();
-        Set genreList = new HashSet();
+        Set<String> genreList = new HashSet<>();
+
         for (AnimationInformation animationInformation : list) {
             genreList.addAll(animationInformation.getGenreList());
         }
