3ec9580d9cae89af36adb911e64c1542871f8cc2
parkgunwook <ss944161269441@gmail.com>
2026-01-06 20:52:34 +0900

    [fix]: fix refreshtoken authorities

--- src/main/java/ahubbe/ahubbe/AHubBeApplication.java
+++ src/main/java/ahubbe/ahubbe/AHubBeApplication.java
@@ -1,5 +1,7 @@
 package ahubbe.ahubbe;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +9,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class AHubBeApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AHubBeApplication.class, args);
